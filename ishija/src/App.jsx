d7cc819f64981fdceaa4b551f1c81dd7dc74c5a4
Lenovo <ishijapriyansha19@gmail.com>
2024-12-31 19:15:11 +0530

    initial commit

---
+++ ishija/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='h-screen w-screen bg-red-800'>
+       <h1 className="text-9xl font-bold  text-white">
+      ISHIJA PRIYANSHA
+    </h1>
+    </div>
+    </>
+  )
+}
+
+export default App

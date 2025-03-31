2a7ea849f6efd3dfc3735ae2df270876f2896fbc
Lenovo <ishijapriyansha19@gmail.com>
2025-04-01 02:25:42 +0530

    minor css changes

--- ishija/src/components/ui/Background-boxes-demo.jsx
+++ ishija/src/components/ui/Background-boxes-demo.jsx
@@ -43,17 +43,18 @@ export default function BackgroundBoxesDemo() {
 
       {/* Button with Fade-in Effect */}
       <motion.button
-        onClick={handleClick}
-        initial={{ opacity: 0, scale: 0.95 }} 
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 1, ease: "easeOut", delay: 0.8 }} 
-        className="relative font-mono mt-5 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white focus:ring-4  
-        before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-white/20 before:transition-all before:duration-500 hover:before:left-0 hover:before:w-full"
-      >
-        <span className="relative px-5 py-2.5 transition-all ease-in duration-1200 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
-          Know more about me!
-        </span>
-      </motion.button>
+  onClick={handleClick}
+  initial={{ opacity: 0, scale: 0.95 }}
+  animate={{ opacity: 1, scale: 1 }}
+  transition={{ duration: 1, ease: "easeOut", delay: 0.8 }}
+  className="relative font-mono mt-5 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white focus:ring-4
+  before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-full before:bg-white/20 before:transition-none hover:before:bg-white/20"
+>
+  <span className="relative px-5 py-2.5 transition-all ease-in duration-1200 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+    Know more about me!
+  </span>
+</motion.button>
+
     </div>
   );
 }

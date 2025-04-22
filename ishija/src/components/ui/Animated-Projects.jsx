205f24ca24856d5bc1f797fbc3939aff0deb2ef3
Lenovo <ishijapriyansha19@gmail.com>
2025-04-22 22:59:14 +0530

    reduce gap

--- ishija/src/components/ui/Animated-Projects.jsx
+++ ishija/src/components/ui/Animated-Projects.jsx
@@ -34,7 +34,7 @@ export const AnimatedTestimonials = ({
   return (
     <div
       className="mx-auto max-w-full px-8 py-28 font-mono antialiased md:px-16 lg:px-24 relative z-0" > 
-      <div className="relative grid grid-cols-1 gap-40 lg:grid-cols-2 ">
+      <div className="relative grid grid-cols-1 lg:gap-40 lg:grid-cols-2 ">
         <div>
           <div className="relative h-[400px] w-[90%]"> 
             <AnimatePresence>
